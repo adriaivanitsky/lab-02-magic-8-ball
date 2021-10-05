b1f20b0bdcea2228e8ea3912d4ef72300fcc923d
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-05 14:52:01 -0700

    created a detailed plan, added array and function, set variables, created event listener for click button

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // import functions and grab DOM elements
-import { answers } from ''./answers.js';
+import { answers } from './answers.js';
+
+const exampleQuestion = document.getElementById('example-question');
+const button = document.getElementById('button');
+const answer = document.getElementById('answer');
 // initialize global state
+const getRandomNumber = (max)=>{
+  return Math.floor(Math.random() * max);
+}
 
 // set event listeners 
+button.addEventListener('click', ()=>{
+  const randomAnswer = getRandomNumber(answers.length);
+  console.log(answers[randomAnswer]); //get the value of the answer
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
-
-
-  const getRandomNumber = (max)=>{
-    return Math.floor(Math.random() * max)
-  }
-
-  const randomAnswer = getRandomNumber(answers.length);
-  console.log(answers[randomAnswer]); 
---
+++ plan.md
@@ -0,0 +1,14 @@
+### What HTML elements do I need?
+* Text area for the question
+* button (static)
+* paragraph to hold the answer (dynamic)
+
+### What State Do I Need?
+* answer
+* randomIdx
+
+### What events am I listening for? what happens when those events are fired?
+* button click
+    * generate a randomIdx
+    * get the value of the answer
+    * display the answer on the page
