8b4cbb1019744f2b59790ecd64cd80ae59297cd1
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-05 13:19:54 -0700

    added html structure in index.html and a list of answers in answers.js

---
+++ answers.js
@@ -0,0 +1,14 @@
+export const answers = [
+    `It is Certain.`,
+    `Yes, definitely.`,
+    `YES.`,
+    `Don't count on it.`,
+    `Signs point to yes.`,
+    `I would tell you, but then I'd have to kill you.`,
+    `Simply put, no.`,
+    `FUCK YES.`,
+    `Very doubtful,`,
+    `Hell nah dude.`,
+    `I think you're asking the wrong question...`,
+    `I dunno dawg. this is so much pressure. magic 8 balls don't get paid, make your own decision `
+]
--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // import functions and grab DOM elements
-
+import { answers } from ''./answers.js';
 // initialize global state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+  const getRandomNumber = (max)=>{
+    return Math.floor(Math.random() * max)
+  }
+
+  const randomAnswer = getRandomNumber(answers.length);
+  console.log(answers[randomAnswer]); 
