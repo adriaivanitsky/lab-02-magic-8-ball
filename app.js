6c054bec68ca4ca379ceafab893bae7f8bee285e
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-08 16:02:21 -0700

    made all text fit inside triangle in JS

--- app.js
+++ app.js
@@ -14,10 +14,20 @@ const getRandomNumber = (max) => {
 button.addEventListener('click', () => {
     const randomAnswer = getRandomNumber(answers.length);
     const randomQuestion = getRandomNumber(exampleQuestions.length);
+
+    // if 4 in the array, then font-size = 0.7rem;
+    // else 
+
     answer.textContent = answers[randomAnswer];
     exampleQuestion.placeholder = exampleQuestions[randomQuestion];
 
+    if(randomAnswer === 4 || randomAnswer === 10){
+        answer.style.fontSize = '0.7rem';
+        answer.style.marginTop = '50px';
+    } else {
+        answer.style.fontSize = '0.9rem';
+        answer.style.marginTop = '100px';
+    }
 });
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
+  // answers 4 and 10 are too large! Let's adjust font-size to 0.7rem
